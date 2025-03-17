e4cc83a5bb71614299af36889e70a251d328d0f8
Henrique Camargo <henrique.camargo@neoway.com.br>
2025-03-17 19:46:28 -0300

    feat: create service controller pizza

---
+++ internal/interfaces/pizza_handler.go
@@ -0,0 +1,42 @@
+package interfaces
+
+import (
+	"net/http"
+
+	"github.com/EusRique/pizzaria-backend/internal/app"
+	"github.com/gin-gonic/gin"
+)
+
+type PizzaHandler struct {
+	service *app.PizzaService
+}
+
+func NewPizzaHandler() *PizzaHandler {
+	return &PizzaHandler{service: app.NewPizzaService()}
+}
+
+func (h *PizzaHandler) CreatePizza(c *gin.Context) {
+	var request struct {
+		Name        string  `json:"name" binding:"required"`
+		Description string  `json:"description" binding:"required"`
+		Price       float64 `json:"price" binding:"required"`
+		ImageURL    string  `json:"image_url"`
+	}
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error creating pizza"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Pizza created successfully!"})
+}
+
+func (h *PizzaHandler) ListPizzas(c *gin.Context) {
+	pizzas, err := h.service.ListPizzas()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error listing pizzas"})
+		return
+	}
+
+	c.JSON(http.StatusOK, pizzas)
+}
