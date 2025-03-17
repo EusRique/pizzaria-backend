7898386a4ab25c88f46cb24bce6305e57087d1b7
Henrique Camargo <henrique.camargo@neoway.com.br>
2025-03-17 19:50:55 -0300

    feat: create server and routes

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/EusRique/pizzaria-backend/config"
+	"github.com/EusRique/pizzaria-backend/internal/interfaces"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	config.InitDB()
+
+	r := gin.Default()
+	pizzaHandler := interfaces.NewPizzaHandler()
+
+	r.POST("/pizzas", pizzaHandler.CreatePizza)
+	r.GET("/pizzas", pizzaHandler.ListPizzas)
+
+	fmt.Println("Server running on port 8080")
+	r.Run(":8080")
+}
