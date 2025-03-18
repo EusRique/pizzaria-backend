f918b1dc6d1e024e0bc40e396edb4b2da2e968d1
Henrique Camargo <henrique.camargo@neoway.com.br>
2025-03-18 14:23:22 -0300

    feat: add rules of required fieldds

--- internal/domain/pizza.go
+++ internal/domain/pizza.go
@@ -1,9 +1,44 @@
 package domain
 
+import (
+	"fmt"
+)
+
 type Pizza struct {
 	ID          uint    `gorm:"primaryKey"`
 	Name        string  `gorm:"type:varchar(255); not null"`
 	Description string  `gorm:"type:text; not null"`
 	Price       float64 `gorm:"type:decimal; not null"`
 	Image       string  `gorm:"type:varchar(255)"`
+}
+
+func (pizza *Pizza) IsValid() error {
+	if pizza.Name == "" {
+		return fmt.Errorf("nome é obrigatório")
+	}
+
+	if pizza.Description == "" {
+		return fmt.Errorf("descrição é obrigatória")
+	}
+
+	if pizza.Price == 0 {
+		return fmt.Errorf("preço é obrigatório")
+	}
+
+	return nil
+}
+
+func NewPizza(name, description string, price float64, image string) (*Pizza, error) {
+	pizza := Pizza{
+		Name:        name,
+		Description: description,
+		Price:       price,
+		Image:       image,
+	}
+
+	if err := pizza.IsValid(); err != nil {
+		return nil, err
+	}
+
+	return &pizza, nil
 }
