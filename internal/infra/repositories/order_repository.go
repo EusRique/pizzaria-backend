d821b1ed824f7a1ab1adf5ab829f85b02ba40a26
Henrique Camargo <henrique.camargo@neoway.com.br>
2025-03-19 11:21:19 -0300

    feat: created order_repository

---
+++ internal/infra/repositories/order_repository.go
@@ -0,0 +1,29 @@
+package repositories
+
+import (
+	"github.com/EusRique/pizzaria-backend/config"
+	"github.com/EusRique/pizzaria-backend/internal/domain"
+	"gorm.io/gorm"
+)
+
+type OrderRepository struct {
+	db *gorm.DB
+}
+
+func NewOrderRepository() *OrderRepository {
+	return &OrderRepository{db: config.DB}
+}
+
+func (r *OrderRepository) CreateOrder(order *domain.Order) error {
+	return r.db.Create(order).Error
+}
+
+func (r *OrderRepository) GetAllOrders() ([]domain.Order, error) {
+	var orders []domain.Order
+	err := r.db.Preload("Items").Find(&orders).Error
+	return orders, err
+}
+
+func (r *OrderRepository) UpdateOrderStatus(id uint, status string) error {
+	return r.db.Model(&domain.Order{}).Where("id = ?", id).Update("status", status).Error
+}
